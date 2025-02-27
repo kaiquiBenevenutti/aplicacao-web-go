05e14c91a9b1e22996f9730708ead026ebef4317
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2025-02-27 19:59:45 -0300

    produtos passado pelo go

--- main.go
+++ main.go
@@ -2,16 +2,34 @@
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Valor      float64
+	Quantidade int
+}
+
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8000", nil)
+
+	log.Fatal(http.ListenAndServe(":8001", nil))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "index.html", nil)
+	produtos := []Produto{
+		{"Cachorro quente", "Muito saboroso", 15.00, 25},
+		{"Camiseta", "Bem bonita", 29.99, 10},
+	}
+
+	err := temp.ExecuteTemplate(w, "index", produtos)
+	if err != nil {
+		http.Error(w, "Erro ao renderizar a página", http.StatusInternalServerError)
+		log.Println("Erro no template:", err)
+	}
 }
