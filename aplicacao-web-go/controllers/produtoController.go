f66a5d1c6d6d23074f32deb5e9097fde8c729bdc
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2025-03-03 15:47:07 -0300

    add controllers

--- aplicacao-web-go/controllers/produtoController.go
+++ aplicacao-web-go/controllers/produtoController.go
@@ -1,21 +1,14 @@
-﻿package main
+﻿package controllers
 
 import (
 	model "aplicacao-web-go/models"
 	"html/template"
-	"log"
 	"net/http"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
-func main() {
-	http.HandleFunc("/", index)
-
-	log.Fatal(http.ListenAndServe(":8001", nil))
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
+func Index(w http.ResponseWriter, r *http.Request) {
 	produtos := model.GetProdutos()
 	temp.ExecuteTemplate(w, "index", produtos)
 }
---
+++ aplicacao-web-go/main.go
@@ -0,0 +1,13 @@
+﻿package main
+
+import (
+	controller "aplicacao-web-go/controllers"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", controller.Index)
+
+	log.Fatal(http.ListenAndServe(":8001", nil))
+}
